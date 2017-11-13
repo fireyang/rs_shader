1e0fdbbb77d6339f6cf4796f97b09cabed0c5d2a
fireyang <shenhuan007@gmail.com>
2017-11-13 16:40:36 +0800

    add iTime

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ extern crate glium;
 // use rs_shader::*;
 use glium::{glutin, Surface};
 use std::path::Path;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::time::Instant;
 
 #[derive(Copy, Clone)]
 struct Vertex {
@@ -20,10 +24,47 @@ const SCREEN: [Vertex; 4] = [
     Vertex{position: [ 1.0, -1.0]}, // Bottom right
 ];
 
-pub static DEFAULT_VERT_SRC_STR: &'static str = include_str!("../shaders/test.vs");
+pub static DEFAULT_VERT_SRC_STR: &'static str = include_str!("../shaders/test2.vs");
 
 const SCREEN_INDICES: [u16; 6] = [0, 1, 2, 0, 2, 3];
 
+// Fragment shader prefix
+const PREFIX: &'static str = "
+    #version 150 core
+
+    // uniform float     iGlobalTime;
+    uniform float     iTime;
+    uniform vec3      iResolution;
+    // uniform vec4      iMouse;
+    // uniform int       iFrame;
+    // uniform sampler2D iChannel0;
+    // uniform sampler2D iChannel1;
+    // uniform sampler2D iChannel2;
+    // uniform sampler2D iChannel3;
+
+    in vec2 fragCoord;
+    out vec4 fragColor;
+";
+
+// Fragment shader suffix
+const SUFFIX: &'static str = "
+    void main() {
+        mainImage(fragColor, fragCoord);
+    }
+";
+
+pub fn format_shader_src(src: &str) -> String {
+    format!("{}\n{}\n{}", PREFIX, src, SUFFIX)
+}
+
+pub fn load_fragment_shader(file: &str) -> io::Result<String> {
+    let mut frag_src_str = String::new();
+    File::open(&Path::new(&file))?.read_to_string(
+        &mut frag_src_str,
+    )?;
+    let s = format_shader_src(&frag_src_str);
+    Ok(s)
+}
 
 fn main() {
     let mut events_loop = glutin::EventsLoop::new();
@@ -42,23 +83,12 @@ fn main() {
 
     let vertex_shader_src = DEFAULT_VERT_SRC_STR;
 
-    let fragment_shader_src = r#"
-        #version 140
-
-        out vec4 color;
-
-        uniform sampler2D tex;
-        in vec2 v_tex_coords;
-
-        void main() {
-            // color = vec4(1.0, 0.0, 0.0, 1.0);
-            vec2 f = (v_tex_coords + vec2(1.0,1.0)) / 2;
-            color = texture(tex, f);
-        }
-    "#;
+    let file = "./shaders/test2.fs";
+    let fragment_shader_src = load_fragment_shader(&file).unwrap();
+    // let fragment_shader_src = fragment_shader_src.as_slice();
 
     let program =
-        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+        glium::Program::from_source(&display, vertex_shader_src, &fragment_shader_src, None)
             .unwrap();
 
 
@@ -70,6 +100,8 @@ fn main() {
 
 
     let mut closed = false;
+    let mut time: f32 = 0.0;
+    let mut start_time = Instant::now();
     while !closed {
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 0.0, 1.0);
@@ -86,10 +118,14 @@ fn main() {
                 &positions,
                 &indices,
                 &program,
-                &uniform! { matrix: matrix , tex: &diffuse_texture},
+                &uniform! { matrix: matrix , tex: &diffuse_texture, iTime: time},
                 &Default::default(),
             )
             .unwrap();
+        let elapsed = get_tm(&start_time);
+        // println!("time: {}", elapsed);
+
+        time = elapsed;
 
         target.finish().unwrap();
 
@@ -105,12 +141,18 @@ fn main() {
     }
 }
 
+fn get_tm(tm: &Instant) -> f32 {
+    let elapsed = tm.elapsed();
+    let elapsed_ms = (elapsed.as_secs() * 1000) + (elapsed.subsec_nanos() / 1000000) as u64;
+    (elapsed_ms as f32) / 1000.0
+}
+
 extern crate image;
 // use image::GenericImage;
 
 use glium::texture::RawImage2d;
 // use glium::texture::ToClientFormat;
-pub static DEFAULT_TEXTURE0_BUF: &'static [u8] = include_bytes!("../textures/01-brickwall.jpg");
+pub static DEFAULT_TEXTURE0_BUF: &'static [u8] = include_bytes!("../textures/black.png");
 
 fn load_texture<'a>(texpath: &'a Option<&str>) -> glium::texture::RawImage2d<'a, u8> {
     // let image = image::load(Cursor::new(&include_bytes!("../book/tuto-14-diffuse.jpg")[..]),
